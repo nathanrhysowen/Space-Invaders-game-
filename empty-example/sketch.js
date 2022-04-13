931dee057c3f0253d08e41bd3ddfc45c643a6211
nathanrhysowen <Nathan2.Owen@live.uwe.ac.uk>
2022-04-13 21:52:19 +0100

    title screen

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,10 +1,17 @@
 let points = 0;
 let lasers = []; //creates array for lasers//
 let enemies = []; // creates array for enemies// 
-let enemy;
+let stage = 0; 
+let font;
+
+
+
+
+
 
 function setup() {
   createCanvas(600,600);
+  rectMode(CENTER);
 
   for(let i = 0; i < 6; i++){ //creates 6 enemies//
     //spawns an enemy at a random x and y above the visible screen//
@@ -16,8 +23,90 @@ function setup() {
   }
 }
 
+
+
+
+
+
+
+
 function draw() {
-  background(220);
+  if (stage == 0){
+    titlescreen();
+  }
+
+  if (stage == 1){
+    level1();
+  }
+
+  if (stage == 2){
+    level2();
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+function titlescreen(){
+background(0);
+//red border//
+stroke(255,0,0);
+noFill()
+strokeWeight(10);
+rect(height/2,width/2,height,width);
+noStroke();
+
+//words//
+fill(255,0,0);
+textSize(55);
+text('SPACE INVADERS', 65, 100);
+
+
+
+fill(255,0,0);
+textSize(30);
+text('PLAY GAME', 220, 280);
+
+strokeWeight(5);
+fill(255,0,0);
+line(180,300,260,300);
+
+
+fill(255,0,0);
+textSize(25);
+text('HOW TO PLAY', 220, 420);
+
+fill(255,0,0);
+textSize(13);
+text('1.Use mouse courser to move along the X-axis.', 176, 440);
+text('2.Use left mouse button to fire lasers.', 200, 460);
+text('3.Defeat all imvaders before they reach the bottom', 165, 480);
+
+noFill()
+stroke(255,0,0);
+strokeWeight(3);
+
+rect(305,270,200,50);
+
+
+}
+
+
+
+
+
+
+
+
+function level1(){
+
+  background(0);
   circle(mouseX,570,25); //creating the player//
   for(let laser of lasers){ //grabs laser from the array//
     rect(laser.x,laser.y,4,20) //creating the laser object//
@@ -27,9 +116,12 @@ function draw() {
   for(let enemy of enemies){ //pulls enemies from array//
     enemy.y += 1; //speed of which the enemies are falling//
     circle(enemy.x,enemy.y,15) //enemy shape//
-    if (enemy.y > height){
-     text("GAME OVER", 200, 300);
-     noLoop();
+     if (enemy.y > height){
+      text("GAME OVER", 200, 300); //if enemy reaches bottom of screen, game loop ends and death screen appears//
+      noLoop();
+     }
+
+  
 
   }
 
@@ -37,18 +129,25 @@ function draw() {
   for (let enemy of enemies){
     for(let laser of lasers){
       if(dist(enemy.x,enemy.y,laser.x,laser.y) < 10){
-         enemies.splice(enemies.indexOf(enemy), 1); //ene
+         enemies.splice(enemies.indexOf(enemy), 1); //when lasers and enemies collide, the objexts dissappear//
          lasers.splice(lasers.indexOf(laser), 1)
-         points +=1;
+          points +=1; //adds one point//
       }
     }
   }
   
-    mousePressed();
-    pointsystem();
-
+    
+     pointsystem();
   
+
 }
+
+
+
+
+
+
+
 
 function mousePressed(){
  //defining the location where the lasers spawn//
@@ -59,11 +158,42 @@ function mousePressed(){
   lasers.push(laser); 
 }
 
+
+
+
+
+
+
+
+
 function pointsystem(){
-  textSize(30);
-  text(points,25,30);
+   textSize(30);
+   text(points,25,30);
 
-}
+   
+   }
+
+ 
 
 
-}
+
+
+
+
+
+
+
+
+function level2(){
+background(0);
+
+stroke(255,0,0);
+noFill()
+strokeWeight(3);
+rect(width/2, height/2, width, height);
+noStroke();
+
+  }
+
+
+
